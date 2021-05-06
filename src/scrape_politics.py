5b5e141a67aa3876eab7b6b8e477805f8d15182e
juleshansen <jules.hansen93@gmail.com>
2021-05-05 23:17:23 -0600

    test scrape of top politics post

--- src/scrape_politics.py
+++ src/scrape_politics.py
@@ -8,8 +8,13 @@ posts = list(reddit.subreddit('politics').top(time_filter='year', limit=1))
 conn = psycopg2.connect('postgres://postgres:password@127.0.0.1:5432/reddit')
 curr = conn.cursor()
 
+post_iter = 0
+comment_iter = 0
+
 for post in posts:
     post.comments.replace_more(limit=None)
+    post_iter += 1
+
     for comment in post.comments.list():
         if comment.author == 'AutoModerator' or comment.author is None:
             continue
@@ -29,7 +34,7 @@ for post in posts:
         '''
         curr.execute(query, json)
         conn.commit()
+        comment_iter += 1
+        print(f'\rStored {comment_iter} comments from {post_iter} posts...')
 
-curr.execute('SELECT COUNT(*) FROM comments;')
-print(f'Pulled {curr.fetchone()[0]} comments')
 conn.close()
